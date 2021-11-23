7d018cdca5b56482563eff61c46e8555c8d2d3ad
vaibhavrr1 <vaibhav.rr1@gmail.com>
2021-11-23 23:57:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,17 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = "simbots",
-    version = "0.0.12",
+    version = "0.0.17",
     description = "Simple bots or Simbots is a library designed to create simple bots using the power of python.",
-    packages =["simbots"],
+    packages =["simbots","simbots\\utils"],
     package_dir = {'':'simbots'},
     classifiers = ["Development Status :: 1 - Planning","Operating System :: OS Independent","License :: OSI Approved :: MIT License","Programming Language :: Python :: 3"],
     install_requires =['sklearn','objectpath'],
     license_files = ('LICENSE.txt'),
     long_description=long_description,
     long_description_content_type='text/markdown',
+    author = "Vaibhav Arora",
+    author_email ="vaibhav.rr1@gmail.com"
 
 
 )
